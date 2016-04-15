c2dce556fe472297a4e2e6d57d6ddecdd9473bcf
DravenPredator <depree.rowland@gmail.com>
2016-04-15 17:28:38 -0400

    Minor changes and adding comments

--- BFS_Test.py
+++ BFS_Test.py
@@ -1,15 +1,26 @@
 from BST import BinSearchTree
 
+
 def main():
     bt = BinSearchTree()
-    data_2 = [5, 4, 10, 3, 11, 12, 6, 7, 2, 1, 20, 8]
     data = [15, 6, 18, 3, 7, 17, 20, 2, 4, 13, 9]
-    for val in data_2:
+    for val in data:
         bt.add_node(val)
-    #print(bt.return_root())
-    #bt.postorder_tree_walk(bt.root)
-    #bt.tree_search(bt.root, 9)
+
+    print(
+        'Welcome to the BST Program!\nIt will now run through a pre set of operations for you using pre created data.')
+    print('(Note: If you want to change any value in the program you will need to modify the test file)\n')
+    print('Data: ', end='')
+    print(data)
+    print('\nBinary Tree by Level (Question 1): ')
     bt.print_tree()
+    print('\nBinary tree pre-order (Question 2): ')
+    bt.preorder_tree_walk(bt.root)
+    print('\nBinary tree post-order (Question 2): ')
+    bt.postorder_tree_walk(bt.root)
+    print('\nBinary tree search looking for the value 9 (Question 3): ')
+    bt.tree_search(bt.root, 9)
+
 
 if __name__ == '__main__':
     main()
